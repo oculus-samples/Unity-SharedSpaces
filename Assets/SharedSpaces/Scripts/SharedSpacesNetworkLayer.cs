0eb84df8c5e390188925b41dd9400cd51dc6a890
Zac D <45545925+pr0-zac@users.noreply.github.com>
2022-07-08 13:36:37 -0700

    Updated license headers

--- Assets/SharedSpaces/Scripts/SharedSpacesNetworkLayer.cs
+++ Assets/SharedSpaces/Scripts/SharedSpacesNetworkLayer.cs
@@ -1,4 +1,6 @@
 // Copyright (c) Facebook, Inc. and its affiliates.
+// Use of the material below is subject to the terms of the MIT License
+// https://github.com/oculus-samples/Unity-SharedSpaces/tree/main/Assets/SharedSpaces/LICENSE
 
 using System;
 using System.Collections;
